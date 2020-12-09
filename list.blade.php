7a046fdeff38f7bcc66cd67208b2290794e387af
alifah99 <55783173+alifah99@users.noreply.github.com>
2020-12-09 21:21:42 +0800

    Create list.blade.php

---
+++ list.blade.php
@@ -0,0 +1,38 @@
+<h1>Following is the list of e-book :</h1>
+    <br>ENJOY THE READING!!<br><br>
+    <h3>
+    Book Title : John Adams <br>
+    ISBN : 0743223136 (ISBN13: 9780743223133) <br>
+    Book Category : History <br>
+    Number of pages : 751 pages
+    </h3><br>
+        <h3>
+        Book Title : Hiroshima <br>
+        ISBN : 0679721037 (ISBN13: 9780679721031) <br>
+        Book Category : History <br>
+        Number of pages : 152 pages
+        </h3><br>
+            <h3>
+            Book Title : The Immortal Rules(Blood of Eden #1) <br>
+            ISBN : 0373210515 (ISBN13: 9780373210510) <br>
+            Book Category : Horror <br>
+            Number of pages : 485 pages 
+            </h3><br>
+               <h3>
+                Book Title : The Walking Dead, Vol. 1: Days Gone Bye(The Walking Dead (Collected Editions) #1) <br>
+                ISBN : 1582406723 (ISBN13: 9781582406725) <br>
+                Book Category : Horror <br>
+                Number of pages : 144 pages
+                </h3><br>
+                    <h3>
+                    Book Title : Harry Potter and the Deathly Hallows(Harry Potter #7) <br>
+                    ISBN : 0545010225 (ISBN13: 9780545010221) <br>
+                    Book Category : Thriller <br>
+                    Number of pages : 759 pages
+                    </h3><br>
+                        <h3>
+                        Book Title : The Hunger Games(The Hunger Games #1) <br>
+                        ISBN : 0439023483 (ISBN13: 9780439023481) <br>
+                        Book Category : Thriller <br>
+                        Number of pages : 374 pages
+                        </h3>

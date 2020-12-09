dc5412d206632bf8f9028b78587b45c4d436f6a5
alifah99 <55783173+alifah99@users.noreply.github.com>
2020-12-09 21:18:06 +0800

    Create about.blade.php

---
+++ about.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-book Sharing Application</title>
+</head>
+<body>
+    <h1> E-Book Sharing Application </h1>
+    <h3> An elegant and user-friendly app for reading ebooks </h3>
+    <p>Ebook Sharing Application makes reading easy. Anytime you want, anywhere you might be. 
+    Search, browse, preview and purchase ebooks to read on your devices</p>
+</body>
+</html>
